7ce20ad5a5442bed134673a0c97afb838f9a24f4
DeepanAnbazhagan <164902865+DeepanAnbazhagan@users.noreply.github.com>
2024-03-30 13:10:27 +0530

    Update SR_FF.v

--- SR_FF.v
+++ SR_FF.v
@@ -18,35 +18,28 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-module SR_FF(q, qb,s,r,clock,reset);
-    input s,r,clock,reset;
-    output reg q, qb;
-always @ (posedge (clock))
-    begin 
-        if (!reset)
-            begin
-               q <= q;
-               qb <=qb;
-            end   
-else
-            begin
-                  //Write the logic for SR Flip Fliop using if-else construct
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-            end
-end  
+module srff_behave(s,r,clk, q, qbar);
+
+input s,r,clk;
+output reg q, qbar;
+
+always@(posedge clk)
+begin
+
+if(s == 1)
+begin
+q = 1;
+qbar = 0;
+end
+else if(r == 1)
+begin
+q = 0;
+qbar =1;
+end
+else if(s == 0 & r == 0) 
+begin 
+q <= q;
+qbar <= qbar
+end
+end
 endmodule
